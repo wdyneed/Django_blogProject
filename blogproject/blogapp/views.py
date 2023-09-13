1ba4aadce165bef747ad5620c39f53a759cd65b6
SGTRyon <chltjdwls326@naver.com>
2023-09-13 15:52:26 +0900

    Feat views.py post 추가

--- blogproject/blogapp/views.py
+++ blogproject/blogapp/views.py
@@ -14,23 +14,6 @@ class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-def register(request):
-    if request.method == 'POST':
-        form = RegistrationForm(request.POST)
-        if form.is_valid():
-            user_name = form.cleaned_data['user_name']
-            user_id = form.cleaned_data['user_id']
-            user_pw = PasswordHasher().hash(form.cleaned_data['user_pw'])
-            user_email = form.cleaned_data['user_email']
-            # 이 부분에서 User 모델을 생성하고 데이터베이스에 저장합니다.
-            user = User(user_name=user_name, user_id=user_id, user_pw=user_pw, user_email=user_email)
-            user.save()
-            return redirect('/')  # 회원가입 성공 페이지로 이동
-    else:
-        form = RegistrationForm()
-    
-    return render(request, './register.html', {'form': form})
-
 # 로그인 하는 곳 (아직 미구현)
 def custom_login(request):
     if request.user.is_authenticated:
@@ -59,3 +42,16 @@ def custom_logout(request):
 def index(request):
     posts = Post.objects.all()
     return render(request, 'index.html', {'posts' : posts})
+
+
+def add_Post_data(request):
+    if request.method == "POST":
+        title = request.POST["title"]
+        content = request.POST["content"]
+        published_date = request.POST["published_date"]
+
+        post = Post(title=title, content=content, published_date=published_date)
+        post.save()
+        return redirect("site")
+
+    return render(request, "blogapp/site.html")
