b8ad7999bdb23aceed061b9e5237872be6c03d0a
UNeedStudy <zlsoq@naver.com>
2023-09-13 09:24:35 +0900

    Update
    static 폴더 위치 변경
    views, forms.py 에서 회원가입 로직을 변경했습니다.

--- blogproject/blogapp/forms.py
+++ blogproject/blogapp/forms.py
@@ -3,6 +3,7 @@ from django.core.exceptions import ValidationError
 from re import match
 import re
 from argon2 import PasswordHasher
+from .models import User
 
 class RegistrationForm(forms.Form):
     user_name = forms.CharField(max_length=50)
@@ -11,13 +12,19 @@ class RegistrationForm(forms.Form):
     confirm_password = forms.CharField(max_length=100, widget=forms.PasswordInput)
     user_email = forms.EmailField(max_length=100)
 
+    def clean_user_id(self):
+        clean_user_id = super().clean().get('user_id')
+        if User.objects.filter(user_id=clean_user_id).exists():
+            raise forms.ValidationError('이미 존재하는 ID입니다.')
+        return clean_user_id
+
     def clean_email(self):
-        email = self.cleaned_data.get('user_email')
+        clean_email = self.cleaned_data.get('user_email')
         # 이메일 유효성 검사 규칙을 여기에 추가
         regex_email = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9_-]+\.[a-zA-Z0-9-.]+$'
-        if not re.match(regex_email, email):
+        if not re.match(regex_email, clean_email):
             raise ValidationError('유효하지 않은 이메일 주소입니다.')
-        return email
+        return clean_email
 
     def clean(self):
         cleaned_data = super().clean()
@@ -31,18 +38,33 @@ class RegistrationForm(forms.Form):
             raise ValidationError("비밀번호와 비밀번호 확인이 일치하지 않습니다.")
 
         
-class LoginForm(forms.Form):                                  
-    user_id = forms.CharField(                             
-        error_messages={                                     
+class LoginForm(forms.Form):                           
+    username = forms.CharField(                           
+        error_messages={                                   
             'required': '아이디를 입력해주세요'
         },
-        max_length=32, label="사용자이름")                    
+        max_length=32, label="사용자이름")                   
     
-    user_pw = forms.CharField(
+    password = forms.CharField(
         error_messages={
             'required': '비밀번호를 입력해주세요'
         },
-        widget=forms.PasswordInput, label="비밀번호")       
+        widget=forms.PasswordInput, label="비밀번호")         
+    
+    def clean(self):                                          
+        cleaned_data = super().clean()
+        username = cleaned_data.get('username')
+        password = cleaned_data.get('password')
+
+        if username and password :
+            try:
+                user = User.objects.get(username=username)
+                if not PasswordHasher(password, user.user_pw):
+                    self.add_error('password', '비밀번호를 틀렸습니다.')   
+                else:
+                    self.user_id = user.id                               
+            except Exception:
+                self.add_error('username', '존재하지 않는 아이디 입니다.')   
     
     
     
--- blogproject/blogapp/views.py
+++ blogproject/blogapp/views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets
 from .models import Post, User
 from .serializers import PostSerializer
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 from .forms import RegistrationForm, LoginForm
 from django.core.exceptions import ValidationError
 from argon2 import PasswordHasher
@@ -19,10 +19,6 @@ def register(request):
             user_name = form.cleaned_data['user_name']
             user_id = form.cleaned_data['user_id']
             user_pw = PasswordHasher().hash(form.cleaned_data['user_pw'])
-            #user_id가 존재하는 경우 메세지를 띄움
-            if User.objects.filter(user_id=user_id):
-                messages.error(request, '이미 존재하는 아이디입니다.')
-                return redirect('register')
             user_email = form.cleaned_data['user_email']
             # 이 부분에서 User 모델을 생성하고 데이터베이스에 저장합니다.
             user = User(user_name=user_name, user_id=user_id, user_pw=user_pw, user_email=user_email)
@@ -36,21 +32,14 @@ def register(request):
 # 로그인 하는 곳 (아직 미구현)
 def custom_login(request):
     if request.method =="POST":
-        form = LoginForm(request.POST)                         
-        if form.is_valid():
-            user_id = form.cleaned_data.get('user_id')
-            user_pw = form.cleaned_data.get('user_pw')
-            if user_id and user_pw :
-                try:
-                    user = User.objects.get(user_id)
-                    if not PasswordHasher().verify(user_pw, user.user_pw):
-                        raise ValidationError('비밀번호를 틀렸습니다.')                               
-                except Exception:
-                    raise ValidationError('존재하지 않는 아이디입니다.')                                              
-                return redirect('/')
-        else:
-            form = LoginForm()                                        
-    return render(request, 'login.html', {'form': form})                                        
+        form = LoginForm(request.POST)                            
+        if form.is_valid():                                      
+            request.session['user'] = form.user_id                 
+            return redirect('/')
+    else:
+        form = LoginForm()                                     
+    
+    return render(request, 'login.html', {'form':form})                                        
     
 # 인덱스 화면 불러오는 함수
 def index(request):
