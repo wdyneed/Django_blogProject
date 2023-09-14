12a58d49a15a79cf30054a98a3adfd44d6a8b9f0
UNeedStudy <zlsoq@naver.com>
2023-09-14 17:59:52 +0900

    Update views.py, index, site_2.html Create timetemplates.py
    날짜 계산하는 커스텀 필터 적용했습니다.

--- blogproject/blogapp/views.py
+++ blogproject/blogapp/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth import authenticate, login, logout
 from bs4 import BeautifulSoup
 from django.core.paginator import Paginator
 from django.conf import settings
-
+from datetime import datetime, timedelta
 from django.http import JsonResponse
 from django.views import View
 from django.core.files.storage import default_storage
@@ -50,7 +50,6 @@ def index(request):
     paginator = Paginator(posts, posts_per_page)
     page_number = request.GET.get('page')
     page_posts = paginator.get_page(page_number)
-    
     return render(request, 'index.html', {'posts' : posts, 'representpost' : representpost, 'page_posts': page_posts})
 
 # 글 작성하는 함수
@@ -84,8 +83,8 @@ def view_post(request, post_id):
     post.view_count += 1
     post.save()
     # 이전/다음 게시물 가져옴
-    previous_post = Post.objects.filter(id__lt=post_id, publish='Y').order_by('-id').first()
-    next_post = Post.objects.filter(id__gt=post_id, publish='Y').order_by('id').first()
+    previous_post = Post.objects.filter(id__lt=post.id, publish='Y').order_by('-id').first()
+    next_post = Post.objects.filter(id__gt=post.id, publish='Y').order_by('id').first()
 
     # 같은 주제인 게시물들 중 최신 글 가져옴
     recommended_posts = Post.objects.filter(topic=post.topic, publish='Y').order_by('-published_date')[:2]
@@ -125,46 +124,66 @@ class image_upload(View):
         # 이미지 업로드 완료시 JSON 응답으로 이미지 파일의 url 반환
         return JsonResponse({'location': file_url})
     
-def create_or_update_post(request, post_id=None):
-    # 글수정 페이지의 경우
-    if post_id:
-        post = get_object_or_404(Post, id=post_id)
+# def create_or_update_post(request, post_id=None):
+#     # 글수정 페이지의 경우
+#     if post_id:
+#         post = get_object_or_404(Post, id=post_id)
     
-    # 글쓰기 페이지의 경우, 임시저장한 글이 있는지 검색 
-    else:
-        post = Post.objects.filter(author_id=request.user.username, publish='N').order_by('-created_at').first()
+#     # 글쓰기 페이지의 경우, 임시저장한 글이 있는지 검색 
+#     else:
+#         post = Post.objects.filter(author_id=request.user.username, publish='N').order_by('-created_at').first()
 
-    # 업로드/수정 버튼 눌렀을 떄
-    if request.method == 'POST':
-        form = PostForm(request.POST, instance=post) # 폼 초기화
-        if form.is_valid():
-            post = form.save(commit=False)
+#     # 업로드/수정 버튼 눌렀을 떄
+#     if request.method == 'POST':
+#         form = PostForm(request.POST, instance=post) # 폼 초기화
+#         if form.is_valid():
+#             post = form.save(commit=False)
 
-            # 게시물 삭제
-            if 'delete-button' in request.POST:
-                post.delete() 
-                return redirect('blog_app:site') 
+#             # 게시물 삭제
+#             if 'delete-button' in request.POST:
+#                 post.delete() 
+#                 return redirect('blog_app:site') 
 
-            if not form.cleaned_data.get('topic'):
-                post.topic = '전체'
+#             if not form.cleaned_data.get('topic'):
+#                 post.topic = '전체'
             
-            # 임시저장 여부 설정
-            if 'temp-save-button' in request.POST:
-                post.publish = 'N'
-            else:
-                post.publish = 'Y'
+#             # 임시저장 여부 설정
+#             if 'temp-save-button' in request.POST:
+#                 post.publish = 'N'
+#             else:
+#                 post.publish = 'Y'
 
-            # 글쓴이 설정
-            post.author_id = request.user.username
+#             # 글쓴이 설정
+#             post.author_id = request.user.username
 
-            post.save()
-            return redirect('blog_app:view_post', post_id=post.id) # 업로드/수정한 페이지로 리다이렉트
+#             post.save()
+#             return redirect('blog_app:view_post', post_id=post.id) # 업로드/수정한 페이지로 리다이렉트
     
-    # 수정할 게시물 정보를 가지고 있는 객체를 사용해 폼을 초기화함
+#     # 수정할 게시물 정보를 가지고 있는 객체를 사용해 폼을 초기화함
+#     else:
+#         form = BlogPostForm(instance=post)
+
+#     template = 'blog_app/write.html'
+#     context = {'form': form, 'post': post, 'edit_mode': post_id is not None, 'MEDIA_URL': settings.MEDIA_URL,} #edit_mode: 글 수정 모드여부
+
+#     return render(request, template, context)
+
+
+def calculate_format_datetime(published_date):
+    current_datetime = datetime.now()
+    time_difference = current_datetime - published_date
+
+    if time_difference.days > 0:
+        # 날짜 차이가 1일 이상인 경우
+        return published_date.strftime("%Y-%m-%d %H:%M")
+    elif time_difference.seconds >= 3600:
+        # 1시간 이상 24시간 미만인 경우
+        hours = time_difference.seconds // 3600
+        return f"{hours}시간 전"
+    elif time_difference.seconds >= 60:
+        # 1분 이상 1시간 미만인 경우
+        minutes = time_difference.seconds // 60
+        return f"{minutes}분 전"
     else:
-        form = BlogPostForm(instance=post)
-
-    template = 'blog_app/write.html'
-    context = {'form': form, 'post': post, 'edit_mode': post_id is not None, 'MEDIA_URL': settings.MEDIA_URL,} #edit_mode: 글 수정 모드여부
-
-    return render(request, template, context)
+        # 1분 미만인 경우
+        return "방금 전"
